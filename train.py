2e999d7d69a568ef2f8287f80d6a4ed474e9fd0b
adityasingh8107 <149563377+adityasingh8107@users.noreply.github.com>
2024-03-03 00:34:34 +0530

    Update train.py

--- train.py
+++ train.py
@@ -16,16 +16,12 @@ from sklearn.metrics import accuracy_score
 
 dataset = load_dataset("sst")
 
-# Load the pretrained Reformer model and tokenizer
 model_name = "google/reformer-crime-and-punishment"
 tokenizer = AutoTokenizer.from_pretrained(model_name, max_length=350)
 
-# Check if the tokenizer has a padding token
 if tokenizer.pad_token is None:
-    # If tokenizer doesn't have a padding token, set it to the end-of-sequence token (eos_token)
     tokenizer.pad_token = tokenizer.eos_token
 
-# Set the padding token
 tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
 model = ReformerForSequenceClassification.from_pretrained(model_name, num_labels=2)  # binary classification for sentiment
@@ -33,18 +29,14 @@ model = ReformerForSequenceClassification.from_pretrained(model_name, num_labels
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-# Define the data preprocessing function
 def preprocess_function(examples):
     return tokenizer(examples["sentence"], truncation=True, padding="max_length")
 
-# Preprocess the dataset
 dataset = dataset.map(preprocess_function, batched=True)
 
-# Split the dataset into train and test sets
 train_dataset = dataset["train"]
 test_dataset = dataset["test"]
 
-# Define the training arguments
 training_args = TrainingArguments(
     output_dir="./output",  # Specify the output directory
     per_device_train_batch_size=2,  # Decrease the batch size for training
@@ -58,7 +50,6 @@ learning_rate = 2e-5  # Adjust as needed
 loss_fn = torch.nn.CrossEntropyLoss()  # For binary classification
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)  # Choose suitable optimizer
 
-# Use the train function to train the model
 train(model, train_dataset, test_dataset, training_args.num_train_epochs, learning_rate, loss_fn, optimizer, device)
 
 print("Training complete!")
