9ae7d5a448890d812c11e77d94c7b7a1db8059c4
adityasingh8107 <149563377+adityasingh8107@users.noreply.github.com>
2024-03-03 09:42:42 +0530

    Update train.py

--- train.py
+++ train.py
@@ -14,7 +14,9 @@ from Model import ReformerForSequenceClassification
 from datasets import load_dataset
 from sklearn.metrics import accuracy_score
 
-dataset = load_dataset("sst")
+dataset = load_dataset("imdb")
+data = dataset['train']['test']
+np.random.shuffle(dataset)
 
 model_name = "google/reformer-crime-and-punishment"
 tokenizer = AutoTokenizer.from_pretrained(model_name, max_length=350)
@@ -30,27 +32,23 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 def preprocess_function(examples):
-    return tokenizer(examples["sentence"], truncation=True, padding="max_length")
+    return tokenizer(examples["text"], truncation=True, padding="max_length")
 
 dataset = dataset.map(preprocess_function, batched=True)
 
 train_dataset = dataset["train"]
 test_dataset = dataset["test"]
 
-training_args = TrainingArguments(
-    output_dir="./output",  # Specify the output directory
-    per_device_train_batch_size=2,  # Decrease the batch size for training
-    per_device_eval_batch_size=2,  # Decrease the batch size for evaluation
-    num_train_epochs=3,
-    evaluation_strategy="epoch",
-    logging_dir="./logs",
-)
+batch_size = 32
+num_batches = len(train_dataset) // batch_size
 
 learning_rate = 2e-5  # Adjust as needed
 loss_fn = torch.nn.CrossEntropyLoss()  # For binary classification
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)  # Choose suitable optimizer
 
-train(model, train_dataset, test_dataset, training_args.num_train_epochs, learning_rate, loss_fn, optimizer, device)
+for i in range(num_batches):
+    batch_data = train_dataset[i * batch_size: (i + 1) * batch_size]
+    train(model, train_dataset, test_dataset, 5, learning_rate, loss_fn, optimizer, device)
 
 print("Training complete!")
 
